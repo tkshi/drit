2c372db1a233974153d3eaafd59e84ad865dd5d9
遠藤孝志 <takashi@endoutakashi-no-MacBook-Air.local>
2016-02-06 07:24:50 +0900

    ok

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
 #!/usr/bin/env node
-
 const exec = require('child_process').exec;
+var funcs = {}
 
-exec('bash bin/' + process.argv[2],  (error, stdout, stderr) => {
-console.log(stdout)
-})
+const shell = (exec_list)=>{
+  exec_list.forEach((exec_str)=>{
+    exec(exec_str, (error, stdout, stderr) => {
+      console.log(stdout)
+    })
+  })
+}
+
+funcs.init = ()=>{
+  shell([
+    'mkdir ~/Dropbox/Git',
+  ])
+}
+
+funcs.create = ()=>{
+  var repo_name = process.argv[3] || process.cwd().split('/').pop()
+  shell([
+    `git init --bare ${process.env.HOME}/Dropbox/Git/${repo_name}.git`,
+    `git remote add drit ${process.env.HOME}/Dropbox/Git/${repo_name}.git`
+  ])
+}
+
+funcs.ls = ()=>{
+  shell([
+    `ls ${process.env.HOME}/Dropbox/Git/ | sed -e "s/\.[^.]*$//"`
+  ])
+}
+
+funcs.clone = ()=>{
+  var targetRepo = process.argv[4] || process.argv[3]
+  shell([
+    `git clone ${process.env.HOME}/Dropbox/Git/${process.argv[3]}.git ${targetRepo}`
+  ])
+}
+
+funcs[process.argv[2]]()
